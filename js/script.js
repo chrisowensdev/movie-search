6156077ea28945c78449483971fa138eb7a03ea0
Chris Owens <guitarrockstarco@gmail.com>
2020-09-06 16:35:03 -0400

    Added poster image

--- js/script.js
+++ js/script.js
@@ -1,20 +1,42 @@
 'use strict';
 
-const searchResults = document.getElementById('searchResults');
+const displaySection = document.querySelector('.display-section');
+
+
+
+
+const get = (url) => {
+    return fetch(url)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            return data;
+        })
+}
+
 const getSearchResults = (query) => {
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API.key}&language=en-US&query=${query}`)
-        .then(res => res.json())
-        .then(data => console.log(data));
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${API.key}&language=en-US&query=${query}`;
+    get(url)
+        .then(res => {
+            res.results.forEach(item => {
+                const imageDiv = document.createElement('div');
+                imageDiv.classList.add('image-div');
+                const image = document.createElement('img');
+                let posterPathUrl;
+                if (item.poster_path) {
+                    image.setAttribute('src', `http://image.tmdb.org/t/p/w185${item.poster_path}`);
+                }
+                imageDiv.appendChild(image);
+                displaySection.appendChild(imageDiv);
+
+
+
+
+                //console.log(item.title, item.release_date);
+            });
+        })
 }
 
 
-
-const displayResults = (data) => {
-    console.log(data)
-    // data.results.map(item => {
-    //     const result = document.createElement('li');
-    //     result.text = item.title;
-    //     searchResults.appendChild(result)
-    // })
-}
-console.log(displayResults(getSearchResults('clerks')))
+getSearchResults('clerks');
