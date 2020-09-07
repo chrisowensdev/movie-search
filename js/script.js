1ff5f6760bd4974811156d0ebbebbbbe4b47ae7a
Chris Owens <guitarrockstarco@gmail.com>
2020-09-07 15:47:40 -0400

    Add Rating system

--- js/script.js
+++ js/script.js
@@ -43,17 +43,27 @@ const getSearchResults = (query) => {
 
                 const releaseDate = document.createElement('p');
                 releaseDate.innerText = `Release Date: ${item.release_date}`;
+
                 const image = document.createElement('img');
                 let posterPathUrl;
                 if (item.poster_path) {
                     image.setAttribute('src', `http://image.tmdb.org/t/p/w185${item.poster_path}`);
                 }
+
+                const rating = document.createElement('div');
+                rating.classList.add('rating');
+                rating.innerHTML = ratingDisplay(item.vote_average);
+
+
                 imageDiv.appendChild(image);
                 movieResult.appendChild(imageDiv);
                 movieResult.appendChild(contentDiv);
                 displaySection.appendChild(movieResult);
                 contentDiv.appendChild(movieTitle);
                 contentDiv.appendChild(releaseDate);
+                contentDiv.appendChild(rating);
+
+
 
 
 
@@ -64,4 +74,36 @@ const getSearchResults = (query) => {
 }
 
 
-//getSearchResults('clerks');
+
+const ratingDisplay = (rating) => {
+    const star = '<i class="fas fa-star"></i>';
+    const halfStar = '<i class="fas fa-star-half-alt"></i>';
+    const emptyStar = '<i class="far fa-star"></i>';
+    console.log(rating);
+    switch (true) {
+        case rating === 10:
+            return star.repeat(5);
+        case rating < 10 && rating > 8:
+            return star.repeat(4) + halfStar;
+        case rating === 8:
+            return star.repeat(4) + emptyStar;
+        case rating < 8 && rating > 6:
+            return star.repeat(3) + halfStar + emptyStar;
+        case rating === 6:
+            return star.repeat(3) + emptyStar.repeat(2);
+        case rating < 6 && rating > 4:
+            return star.repeat(2) + halfStar + emptyStar.repeat(2);
+        case rating === 4:
+            return star.repeat(2) + emptyStar.repeat(3);
+        case rating < 4 && rating > 2:
+            return star + halfStar + emptyStar.repeat(3);
+        case rating === 2:
+            return star + emptyStar.repeat(4);
+        case rating < 2 && rating > 0:
+            return halfStar + emptyStar.repeat(4);
+        default:
+            return 'No Rating';
+
+
+    }
+}
